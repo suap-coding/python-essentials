9f4d4d75aaafe03a85f1219c0dc470aee964d9b1
suap <suap.coding@gmail.com>
2021-07-11 13:36:46 +0200

    Some OOP action.

---
+++ more-advanced-concepts/oop/dog.py
@@ -0,0 +1,36 @@
+class Dog:
+    __slots__ = ['__name', '__age', '__race']
+    
+    def __init__(self, name, age=None, race=None):
+        self.__name = name
+        self.__age = age
+        self.__race = race        
+
+    def __repr__(self):
+        if self.__age == None and self.race == None:
+            return self.name
+        elif self.__age == None:
+            return f"{self.name}, dog of race {self.race}."            
+        elif self.__race == None:
+            return f"{self.name}, {self.age} years old dog."
+        else:
+            return f"{self.name}, {self.age} years old {self.race}."
+        
+    def get_name(self):
+        return self.__name
+    
+    def change_name(self, new_name):
+        self.__name = new_name
+    
+    def get_age(self):
+        return self.__age
+    
+    def get_race(self):
+        return self.__race
+    
+    name = property(get_name, change_name)
+    age = property(get_age)
+    race = property(get_race)
+        
+    def bark(self):
+        print("WOOF!")    
---
+++ more-advanced-concepts/oop/person.py
@@ -0,0 +1,57 @@
+from dog import Dog
+
+class Person:
+    __slots__ = ['__name', '__age', '__gender', '__partner', '__nr_of_kids', '__dog']
+    
+    def __init__(self, name: str, age: int=None, gender=None, partner=None, nr_of_kids: int=None):
+        if partner != None:
+            assert type(partner) == Person
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        self.__partner = partner
+        self.__nr_of_kids = nr_of_kids
+
+    def is_hot(self):
+        if self.__gender == None:
+            return False
+        elif(self.__gender == 'female'):
+            return 17 <= self.__age < 60
+        else:
+            return False
+        
+    def is_handsome(self):
+        if self.__gender == None:
+            return False
+        elif(self.__gender == 'male'):            
+            return 20 <= self.__age < 80
+        else:
+            return False
+    
+    # @typechecked
+    def assign_dog(self, dog: Dog):
+            self.__dog = dog
+        
+    def get_dog(self):
+        return self.__dog
+    
+    # @typechecked
+    def assign_partner(self, partner):
+        assert type(partner) == Person
+        self.__partner = partner
+    
+    def get_partner(self):
+        return self.__partner
+    
+    dog = property(get_dog, assign_dog)
+    partner = property(get_partner, assign_partner)
+
+    def __repr__(self):
+        repr = self.__name
+        if self.__age != None:
+            repr += f", age {self.__age}"
+        if self.is_hot():
+            repr += ", hot"
+        elif self.is_handsome():
+            repr += ", handsome"
+        return repr
